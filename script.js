44cc508f8606fe68b43f60aea4424fed1fb3e739
denart ifurung <dfurung@hotmail.com>
2022-10-25 02:21:23 -0400

    worked on pseudocode. added password arrays

--- script.js
+++ script.js
@@ -15,18 +15,38 @@
 // THEN the password is either displayed in an alert or written to the page
 
 
+// click generate password THEN PROMPT to select length
 
+// TF length is between 8 and 129 THEN Prompt
+// ELSE"must choose between 8 - 129 charachters"
 
-// What are the variables?
-  // we need special charachters
-  // we need numbers
-  // we need letters
-// we need to randomly select each variablw
-// there needs to be a min of 3 and a max of 128
-// prompt for special charachters
+//PROMPT "would you like to include lowercase letters"
+  // IF true then PROMPT and assign ELSE "???"
 
-// Why did they give us .value?
-// What is the password input function?
+//PROMPT "would you like to include upperrcase letters"
+  // IF true then PROMPT and assign  ELSE "???"
+
+//PROMPT "would you like to include numbers"
+  // IF true then PROMPT and assign ELSE "???"
+
+//PROMPT "would you like to special charachters"
+  // IF true then PROMPT and assign ELSE "???"
+
+// list password variables/arrays
+lower = ["a", "b", "c", "d","e", "f", "g", "h","i", "j", "k", "l","m", "n", "o", "p","q", "r", "s", "t","u", "v", "w", "x","y", "z"];
+upper = ["A", "B", "C", "D","E", "F", "G", "H","I", "J", "K", "L","M", "N", "O", "P","Q", "R", "S", "T","U", "V", "W", "X","Y", "Z"];
+number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+special = ["!", "@", "#", "$","%", "^", "&", "*","(", ")", "?"];
+
+//How do we assign these values???? FOR loop??
+
+// generate random password based on arrays
+  //IF include upper is true then
+  //IF include lower is true then
+  //IF include number is true then
+  //IF include special is true then
+  
+
 
 
 
